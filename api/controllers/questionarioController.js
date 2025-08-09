b702fcfc3291ce1381f0b63b6e7ccfe6979f160a
Pauloglhr <pauloglhr@outlook.com>
2025-08-09 18:21:18 -0300

    feat: isolado lógica de controller

--- api/controllers/questionarioController.js
+++ api/controllers/questionarioController.js
@@ -1,12 +1,23 @@
 const questionarioModel = require("../models/questionarioModel");
 
 class QuestionarioController {
-  buscar() {
-    return questionarioModel.listarPerguntas();
+  buscar(req, res) {
+    questionarioModel
+      .listarPerguntas()
+      .then((perguntas) => res.status(200).json(perguntas))
+      .catch((error) => res.status(400).json(error.message));
   }
 
-  criar(novaReposta) {
-    return questionarioModel.criarResposta(novaReposta);
+  criar(req, res) {
+    const { respostas } = req.body;
+    const respostasFormatadas = respostas.map((resposta) => {
+      return [resposta[0], resposta[1], resposta[2]];
+    });
+
+    questionarioModel
+      .criarResposta(respostasFormatadas)
+      .then((respostaCriada) => res.status(201).json(respostaCriada))
+      .catch((error) => res.status(400).json(error.message));
   }
 }
 
--- api/routers/questionarioRoute.js
+++ api/routers/questionarioRoute.js
@@ -1,27 +1,9 @@
 const { Router } = require("express");
+const questionarioController = require("../controllers/questionarioController");
 const router = Router();
 
-const questionarioController = require("../controllers/questionarioController");
+router.get("/questionario", questionarioController.buscar);
 
-router.get("/questionario", (req, res) => {
-  const resposta = questionarioController.buscar();
-  resposta
-    .then((perguntas) => res.status(200).json(perguntas))
-    .catch((error) => res.status(400).json(error.message));
-});
-
-router.post("/questionario", (req, res) => {
-  const { respostas } = req.body;
-
-  //divide o corpo de req.body em [envioId, id_pergunta, resposta]
-  const respostasFormatadas = respostas.map((resposta) => {
-    return [resposta[0], resposta[1], resposta[2]];
-  });
-
-  const resposta = questionarioController.criar(respostasFormatadas);
-  resposta
-    .then((respostaCriada) => res.status(201).json(respostaCriada))
-    .catch((error) => res.status(400).json(error.message));
-});
+router.post("/questionario", questionarioController.criar);
 
 module.exports = router;
